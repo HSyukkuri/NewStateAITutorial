0ee651c6be59201a441b16e0967b4acc28b318d7
HSyukkuri <happisanraizu@gmail.com>
2022-09-12 01:15:58 +0900

    EP10
    生活サイクルシステムを生成

--- StateAI/Assets/Scripts/PlayerController.cs
+++ StateAI/Assets/Scripts/PlayerController.cs
@@ -2,23 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
+using System;
+using UnityEngine.UI;
+using System.Linq;
 
 public class PlayerController : MonoBehaviour
 {
-    public Transform point1;
-    public Transform point2;
+    public Transform point_Living;
+    public Transform point_Bed;
+    public Transform point_Table;
+    public Transform point_Toilet;
+    public Text text;
 
     enum State {
-        MoveToPoint1,
-        StopOnPoint1,
-        MoveToPoint2,
-        StopOnPoint2,
+        MoveToDestination,
+        Eating,
+        Sleeping,
+        SitOnToilet,
+        DoNothing,
     }
 
-    State currentState = State.MoveToPoint1;
+    State currentState = State.MoveToDestination;
+    State targetState = State.DoNothing;
     bool stateEnter = false;
     float stateTime = 0f;
+
+    enum Desire {
+        Toilet,
+        Eat,
+        Sleep,
+    }
+
+    Dictionary<Desire, float> desireDictionary = new Dictionary<Desire, float>();
+
 
     void ChangeState(State newState) {
         currentState = newState;
@@ -34,6 +50,13 @@ public class PlayerController : MonoBehaviour
         navMeshAgent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
         Cursor.lockState = CursorLockMode.Locked;
+
+        foreach(Desire desire in Enum.GetValues(typeof(Desire))) {
+            desireDictionary.Add(desire, 0f);
+        }
+
+        ChangeState(State.MoveToDestination);
+
     }
 
     private void Update() {
@@ -43,62 +66,126 @@ public class PlayerController : MonoBehaviour
 
         animator.SetFloat("PlayerSpeed", speed);
 
+        if(currentState != State.Eating) {
+            desireDictionary[Desire.Eat] += Time.deltaTime / 5f;
+        }
+
+        if(currentState != State.Sleeping) {
+            desireDictionary[Desire.Sleep] += Time.deltaTime / 10f;
+        }
+
+        if(currentState != State.SitOnToilet) {
+            desireDictionary[Desire.Toilet] += Time.deltaTime / 7f;
+        }
+
+        var sortedDesire = desireDictionary.OrderByDescending(i => i.Value);
+
+        text.text = "";
+
+        foreach(var sortedDesireElement in sortedDesire) {
+            text.text += sortedDesireElement.Key.ToString() + ":" + sortedDesireElement.Value + "\n";
+        }
+
+
+
+
 
         switch (currentState) {
 
-            case State.MoveToPoint1: {
+            case State.MoveToDestination: {
                     if (stateEnter) {
-                        navMeshAgent.SetDestination(point1.position);
+
+                        var topDesireElement = sortedDesire.ElementAt(0);
+
+                        if(topDesireElement.Value >= 1f) {
+
+                            switch (topDesireElement.Key) {
+
+                                case Desire.Eat:
+                                    navMeshAgent.SetDestination(point_Table.position);
+                                    targetState = State.Eating;
+                                    break;
+
+                                case Desire.Sleep:
+                                    navMeshAgent.SetDestination(point_Bed.position);
+                                    targetState = State.Sleeping;
+                                    break;
+
+                                case Desire.Toilet:
+                                    navMeshAgent.SetDestination(point_Toilet.position);
+                                    targetState = State.SitOnToilet;
+                                    break;
+                            }
+                        }
+                        else {
+                            navMeshAgent.SetDestination(point_Living.position);
+                            targetState = State.DoNothing;
+                        }
+
+
                     }
 
+                    //–Ú“I’n‚É‚½‚Ç‚è’…‚¢‚½
                     if(navMeshAgent.remainingDistance <= 0.01f && !navMeshAgent.pathPending) {
-                        ChangeState(State.StopOnPoint1);
+                        ChangeState(targetState);
                         return;
                     }
 
                     return;
+                }
+
+            case State.DoNothing: {
+                    if (stateEnter) {
+
+                    }
+
+                    if(sortedDesire.ElementAt(0).Value >= 1) {
+                        ChangeState(State.MoveToDestination);
+                        return;
+                    }
+
+
+                    return;
+                }
+
+            case State.Eating: {
+                    if (stateEnter) {
+
+                    }
+
+                    if(stateTime >= 3f) {
+                        desireDictionary[Desire.Eat] = 0f;
+                        ChangeState(State.MoveToDestination);
+                    }
+
+                    return;
+                }
+
+            case State.Sleeping: {
+                    if (stateEnter) {
+
+                }
+
+                if (stateTime >= 5f) {
+                    desireDictionary[Desire.Sleep] = 0f;
+                    ChangeState(State.MoveToDestination);
+                }
+
+                return;
             }
 
-            case State.StopOnPoint1: {
+            case State.SitOnToilet: {
                     if (stateEnter) {
 
                     }
 
-                    if(stateTime >= 3f) {
-                        ChangeState(State.MoveToPoint2);
-                        return;
+                    if (stateTime >= 3f) {
+                        desireDictionary[Desire.Toilet] = 0f;
+                        ChangeState(State.MoveToDestination);
                     }
 
                     return;
                 }
-
-            case State.MoveToPoint2: {
-                    if (stateEnter) {
-                        navMeshAgent.SetDestination(point2.position);
-                    }
-
-                    if (navMeshAgent.remainingDistance <= 0.01f && !navMeshAgent.pathPending) {
-                        ChangeState(State.StopOnPoint2);
-                        return;
-                    }
-
-                    return;
-                }
-
-            case State.StopOnPoint2: {
-                    if (stateEnter) {
-
-                    }
-
-                    if(stateTime >= 3f) {
-                        ChangeState(State.MoveToPoint1);
-                        return;
-                    }
-
-
-                    return;
-                }
-
         }
     }
 
